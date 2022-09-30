0728506a836ec76f27565a0a2c57a0e8e60aba27
wieceslaw <vyache-nov@mail.ru>
2022-09-30 23:55:56 +0300

    init

---
+++ handler.php
@@ -0,0 +1,79 @@
+<?php 
+session_start();
+if (!isset($_SESSION['history'])) {
+    $_SESSION['history'] = array();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $path =  parse_url($_SERVER['REQUEST_URI'])['path'];
+    $start_time = microtime(true);
+    if ($path == '/lab1/handler.php/hit') { // TODO: change path
+        if (validate($_GET['x'], $_GET['y'], $_GET['r'])) {
+            $hit = hitted($_GET['x'], $_GET['y'], $_GET['r']) ? 'hit' : 'miss';
+            $result = array(
+                'datetime' => time(),
+                'delay' => round((microtime(true) - $start_time) * 1000, 2),
+                'x' => $_GET['x'],
+                'y' => $_GET['y'],
+                'r' => $_GET['r'],
+                'result' => $hit
+            );
+            echo json_encode($result);
+            save($result);
+        } else {
+            http_response_code(400);
+            echo 'Bad request';
+            exit(400);
+        }
+    } elseif ($path == '/lab1/handler.php/history') {
+        echo json_encode($_SESSION['history']);
+    } else {
+        http_response_code(404);
+        echo 'Not found';
+        exit(404);
+    }
+}
+
+function save($element) {
+    array_push($_SESSION['history'], $element);
+}
+
+function validate($x, $y, $r) {
+    if (
+        (isset($x) && isset($y) && isset($r)) &&
+        is_numeric($x) &&
+        is_numeric($y) &&
+        is_numeric($r) && 
+        ($x >= -5 && $x <= 3) &&
+        ($y >= -5 && $y <= 5) &&
+        ($r >= 1 && $r <= 5)
+        ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function hitted($x, $y, $r) {
+    if ($x <= 0 && $y >= 0) {
+        if (sqrt(pow($x, 2) + pow($y, 2)) < $r / 2) {
+            return true;
+        } else {
+            return false;
+        }
+    } elseif ($y <= 0 && $x <= 0) {
+        if (($x > (-$r / 2)) && ($y > -$r)) {
+            return true;
+        } else {
+            return false;
+        }
+    } elseif ($x >= 0 && $y <= 0) {
+        if ($y >= ($x - $r)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
---
+++ script.js
@@ -0,0 +1,124 @@
+const form = document.querySelector('#graphForm')
+const y = document.querySelector('#y')
+const r = document.querySelector('#r')
+const error = document.querySelector('#error')
+const table = document.querySelector('#table-body')
+
+
+function handleError(errorMessage) {
+    error.textContent = errorMessage
+}
+
+function formatFloat(val) {
+    return val.replace(',', '.')
+}
+
+function getY() {
+    return formatFloat(y.value)
+}
+
+function getX() {
+    return $('input[name=x]:checked', '#graphForm').val()
+}
+
+function getR() {
+    return r.value
+}
+
+function formatDate(date) {
+    let options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    return date.toLocaleString('ru', options)
+}
+
+function parse_table_element(data) {
+    return `
+    <tr'>
+        <td>${formatDate(new Date(data['datetime'] * 1000))}</td>
+        <td>${data['delay']} ms</td>
+        <td>${data['x']}</td>
+        <td>${data['y']}</td>
+        <td>${data['r']}</td>
+        <td>${data['result']}</td>
+    </tr>
+    `
+}
+
+function add_element_to_table(data) {
+    tableItem = parse_table_element(data)
+    table.insertAdjacentHTML('afterbegin', tableItem)
+}
+
+function add_elements_to_table(elements) {
+    console.log('elements:', elements)
+    for (const element of JSON.parse(elements)) {
+        add_element_to_table(element)
+    }
+}
+
+function sendData(event) {
+    // validation
+    if (!getY()) {
+        console.log("can not be empty")
+        handleError("Y can not be empty")
+        return
+    }
+    if (!+getY()) {
+        console.log("y must be number")
+        handleError("Y must be a number")
+        return
+    }
+    if (+getY() > 5 || +getY() < -5) {
+        console.log("y must be lower than 5 and higher then -5")
+        handleError("Y must be lower than 5 and higher then -5")
+        return
+    }
+    // sending
+    formData = {
+        'x': getX(),
+        'y': getY(),
+        'r': getR()
+    }
+    $.ajax({
+        url: 'handler.php/hit',
+        data: formData,
+        processData: true,
+        mimeType: 'multipart/form-data',
+        type: 'GET',
+        success: function(data){
+            add_element_to_table(JSON.parse(data))
+        },
+        error: function(data) {
+            alert(data)
+        }
+    })
+}
+
+function fetchHistory() {
+    $.ajax({
+        url: 'handler.php/history',
+        processData: false,
+        type: 'GET',
+        success: function(data){
+            add_elements_to_table(data)
+        },
+        error: function(data) {
+            alert(data)
+        }
+    })
+}
+
+form.addEventListener('submit', (event) => {
+        console.log("submitting")
+        event.preventDefault();
+        sendData(event);
+    }
+)
+
+fetchHistory();
